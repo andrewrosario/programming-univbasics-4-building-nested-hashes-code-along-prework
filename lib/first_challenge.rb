c6328badcb4a65de5a3718c43d16fa7e75ec31c7
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-13 22:57:36 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,33 +2,19 @@ def first_challenge
    epic_tragedy = {
      montague: {
        patriarch: {
-         name: "Lord Montague",
-         age: "53"
        },
        matriarch: {
-         name: "Lady Montague",
-         age: "54"
        },
        hero: {
-         name: "Romeo",
-         age: "15",
-         status: "Alive"
        },
        hero_friends: []
      },
      capulet: {
        patriarch: {
-         name: "Lord Capulet",
-         age: "51"
        },
        matriarch: {
-         name: "Lady Capulet",
-         age: "51"
        },
        heroine: {
-         name: "Juliet",
-         age: "15",
-         status: "Alive"
        },
        heroine_friends: []
      }
